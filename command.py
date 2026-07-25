f7eeb589cadc53c2637f3e70825fcb474694cb7b
P N Harish kumaran <pnharishkumaran@gmail.com>
2026-07-25 22:27:53 +0530

    improved opening applicatios and fixing bugs

--- command.py
+++ command.py
@@ -4,38 +4,21 @@ import os
 import pyautogui
 from speech import speak
 from datetime import datetime
+import psutil
 
 
-apps = {
-    "chrome": "chrome",
-    "notepad": "notepad",
-    "calculator": "calc",
-    "paint": "mspaint",
-    "word": "winword",
-    "excel": "excel",
-    "powerpoint": "powerpnt",
-    "command prompt": "cmd",
-    "file explorer": "explorer"
-}
 def get_date():
     today=datetime.now().strftime("%d %B %Y")
     return f"today date is {today}"
 
 
 def process_command(command):
-    if "open youtube" in command:
-        speak("Opening Youtube")
-        webbrowser.open("https://www.youtube.com")
-    elif "open google" in command:
-        speak("Opening google")
-        webbrowser.open("https://www.google.com")
-    elif "time" in command:
+   
+    if "time" in command:
         current = datetime.now().strftime("%I:%M %p")
         print(f"The current time is {current}")
         speak(f"The curren time is {current}")
-    elif "open notepad" in command:
-        speak("Opening Notepad")
-        os.system("notepad")
+  
     elif "date" in command:
         date=get_date()
         print(date)
@@ -44,9 +27,7 @@ def process_command(command):
         search=command.replace("search","").strip()
         speak(f"searching {search}")
         webbrowser.open(f"https://www.google.com/results?search_query={search} ")
-    elif "open camera" in command:
-        speak("opening camera")
-        os.system("start microsoft.windows.camera:")
+
     elif "take screenshot" in command:
         speak("Taking screenshot")
         screenshot=pyautogui.screenshot()
@@ -59,14 +40,32 @@ def process_command(command):
             file.write(note+"\n")
 
         speak("Okay i have saved your note")
-    elif command.startswith("open "):
-        app_name = command.replace("open", "").strip()
 
-        if app_name in apps:
-            speak(f"Opening {app_name}")
-            os.system(apps[app_name])
-        else:
-            speak("Sorry, I don't know that application.")
+    elif "ram usage" in command:
+        ram=psutil.virtual_memory()
+        used=ram.percent
+        speak(f"RAM Usage in {used} percent")
+        
+    elif "cpu usage" in command:
+        cpu=psutil.cpu_percent(interval=1)
+        speak(f"CPU usage in {cpu} percent")
+
+    elif "disk usage " or "storage" in command:
+        disk=psutil.disk_usage("/")
+        total=round(disk.total/(1024**3),2)
+        free=round(disk.free/(1024**3),2)
+        used=disk.percent
+        speak(f"Disk usage in {used} percent\nTotal storage is {total} gigabytes\nFree storage is {free} gigabytes")
+
+
+    elif command.startswith("open "):
+        name = command.replace("open", "").strip()
+        try:
+            os.system(f"start {name}")
+            speak(f"opening {name}")
+        except:
+            webbrowser.open("https://www.{name}.come")
+            speak(f"opening {name}")
 
     elif "show my notes" in command:
         with open("notes.txt","a") as file:
@@ -76,13 +75,10 @@ def process_command(command):
             speak(notes)
         else:
             speak("you have no notes")
-
-
     elif "search youtube for " in command:
         query=command.replace(f"search youtube for","").strip()
         speak(f"searching youtube for {query}")
         webbrowser.open (f"https://www.youtube.com/results?search_qeary={query} ")
-
 
     elif "open calculater" in command:
         speak("Opening calculater")
--- main.py
+++ main.py
@@ -2,36 +2,40 @@ from speech import speak,listen
 from command import process_command
 from ai import ask_ai
 from greeting import wish
-print("====================")
-print("      AURA AI       ")
-print("====================")
-
-print("Starting AI assistant......")
-greeting=wish()
-
-speak(f"{greeting}, I am Aura, How can i help you ")
-
-
+print("===========================")
+print("      AURA AI ASSISTANT    ")
+print("      POWERED BY GEMINI    ")
+print("===========================")
+print()
+print('Say " Hello Aura " to wake up the voice assistant ')
 while True:
-    command=listen()
+    wake_word=listen()
 
-    if command =="":
-        continue
-    print("You :",command)
+    if wake_word and "hello aura" in wake_word.lower():
+        greeting=wish()
 
-    if "exit" in command:
-        speak("Goodbye have a nice day")
-        break
+        speak(f"{greeting}, I am Aura, How can i help you ")
+        command=listen()
 
-    if "open youtube" in command or"open google"in command or "time" in command or "date" in command or "open notepad " in command or "open calculator" in command or "open camera" in command or "search youtube for" in command or "search" in command or "take screenshot" in command or "remember" in command or "show my notes" in command or "open" in command :
-        process_command(command)
+        speak
 
-    else:
-        try:
+        if command =="":
+            continue
+        print("You :",command)
 
-            response=ask_ai(command)
-            print("Aura :",response)
-            speak(response)
-        except Exception as e:
-            print("Error :", e)
-            print("Sorry i couldn't get response from the AI")
+        if "exit" in command:
+            speak("Goodbye have a nice day")
+            break
+
+        if "open youtube" in command or"open google"in command or "time" in command or "date" in command or "open notepad " in command or "open calculator" in command or "open camera" in command or "search youtube for" in command or "search" in command or "take screenshot" in command or "remember" in command or "show my notes" in command or "open" in command or "ram usage " in command or "CPU usage " in command or "open website" :
+            process_command(command)
+
+        else:
+            try:
+
+                response=ask_ai(command)
+                print("Aura :",response)
+                speak(response)
+            except Exception as e:
+                print("Error :", e)
+                print("Sorry i couldn't get response from the AI")
